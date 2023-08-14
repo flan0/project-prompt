9a22a1bd0711997dcb66e85e2aa90eaf41455ec6
flan <mohamad99tv@gmail.com>
2023-08-14 13:48:11 +0100

    update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,71 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signOut, signIn, useSession, getProviders } from "next-auth/react";
+
 const Nav = () => {
-  return <nav className=""></nav>;
+  const userIsLoggedIn = true;
+  const [Providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          width="30"
+          height="30"
+          alt="Promptopia Logo"
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+      {/* desktop navigation */}
+      <div className="sm:flex hidden">
+        {userIsLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-propmt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full "
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {Providers &&
+              Object.values(Providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>;
+              })}
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
